7d8783348ee15832592ff8e45dcf9b6f36cd5197
ajwhatson <ajwatson@bu.edu>
2019-05-15 16:37:36 -0400

    -commit3

--- caseStudy/services/src/test/java/resources/CompanyResourceTest.java
+++ caseStudy/services/src/test/java/resources/CompanyResourceTest.java
@@ -15,7 +15,26 @@
  */
 
 package resources;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.*; //////////////////////////
+import resources.*; ///////////////////////////
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.test.JerseyTest;
+import org.junit.Assert;
+import org.junit.Test;
+import utility.FileHelper;
 
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.Application;
+import javax.ws.rs.core.Response;
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.*;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static utility.FileHelper.DATEFORMAT;
 /**
  * Write your tests for the Company Resource here
  */
@@ -25,5 +44,29 @@ public class CompanyResourceTest {
     // Think about both positive and negative test cases:
     // What happens if no inputs are passed?
     // What happens if the input is null?
+    private static ObjectMapper mapper = new ObjectMapper();
+
+
+    @Override
+    protected Application configure() {
+        return new ResourceConfig(
+                Example2Resource.class,
+                Example3Resource.class,
+        );
+    }
+    public void testGetCompanyInfo() {
+        List<Company> apple = target().path("/services/company/AAPL").request().get(List.class);
+        String other = target().path("/services/company/random").request().get(String.class);
+
+
+        assertEquals("Apple Inc.", apple.getName());
+        assertEquals("Cupertino", apple.getHeadquartersCity());
+        assertEquals("CA", apple.getHeadquartersStateorCountry());
+        assertEquals(116000, apple.getNumberOfEmployees());
+        assertEquals("Technology", apple.getSector());
+        assertEquals("Semiconductor Machinery Manufacturing", apple.getIndustry());
+        assertEquals(other, "No matches for a company called random")
+    }
+
 
 }
